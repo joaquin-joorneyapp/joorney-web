6034912a493963af210dcbf90701fa939cd4f5f8
Joaquin Crippa <joaquincrippa@gmail.com>
2026-03-21 10:41:54 -0300

    feat: add public route group layout

---
+++ src/app/(public)/layout.tsx
@@ -0,0 +1,23 @@
+import Box from '@mui/material/Box';
+import * as React from 'react';
+import Navbar from '@/components/Navbar';
+
+export default function PublicLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <Navbar />
+      <Box
+        component="main"
+        sx={{
+          bgcolor: 'background.default',
+          mt: ['48px', '56px', '64px'],
+          marginX: ['0.5rem', '1rem', '4rem'],
+          px: 1,
+          pt: 3,
+        }}
+      >
+        {children}
+      </Box>
+    </>
+  );
+}
